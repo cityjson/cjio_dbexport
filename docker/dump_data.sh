aa765da0ccd359785e1fbfab8dc7370671fda516
Balázs Dukai <balazs.dukai@gmail.com>
2021-03-03 14:50:55 +0100

    Dump test data from container

--- docker/dump_data.sh
+++ docker/dump_data.sh
@@ -1,10 +1,23 @@
 #! /bin/bash
 
 # port 5557 points to the postgres 10 database
-PGPASSWORD="cjdb_test" pg_dump -U cjdb_tester -p 5557 -h localhost --dbname="db3dnl" \
+#PGPASSWORD="cjdb_test" pg_dump -U cjdb_tester -p 5558 -h localhost --dbname="db3dnl" \
+#  --clean \
+#  --if-exists \
+#  --create \
+#  --no-owner \
+#  --no-privileges \
+#  | gzip -9 > /data/3D_basisvoorziening/db3dnl/db3dnl_sample.sql.gz
+
+docker exec \
+db3dnl_postgis_13_30_1 \
+  pg_dump \
+  -U cjdb_tester \
+  -p 5432 \
+  -h localhost \
+  --dbname="db3dnl" \
   --clean \
   --if-exists \
-  --create \
   --no-owner \
   --no-privileges \
-  | gzip -9 > /data/3D_basisvoorziening/db3dnl/db3dnl_sample.sql.gz
+  --create | gzip -c > "/tmp/db3dbag_sample.sql.gz";
